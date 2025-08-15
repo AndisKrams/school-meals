88dd12c9c11ec992dee6917227d18cf00adee7d8
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-15 12:32:36 +0300

    Add secret key

--- menu_project/settings.py
+++ menu_project/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+_9dc+d1g4@1i(%@d32e0jv%f#fhd&c^^he%i!+l8ny#e-56g'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com', 
+                 '127.0.0.1',] 
 
 
 # Application definition
