22c37714ad81df6073f3c3737ec3bbd4d3fb4006
AndisKrams <523ku12i@students.codeinstitute.net>
2025-08-15 14:16:24 +0300

    Add Meal class

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Meal(models.Model):
+    meal_date = models.DateField(unique=True)
+    meal_choice1 = models.CharField(max_length=200)
+    meal_choice2 = models.CharField(max_length=200)
+    meal_choice3 = models.CharField(max_length=200, blank=True)
+    meal_choice4 = models.CharField(max_length=200, blank=True)
